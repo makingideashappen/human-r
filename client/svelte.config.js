129008a2a668389a41c3fb71984556ff57078cc7
DESKTOP-7TJI3IV\DELL <makingideashappenreal@gmail.com>
2024-10-29 17:04:01 +0100

    no message

--- client/svelte.config.js
+++ client/svelte.config.js
@@ -1,6 +1,6 @@
 import { mdsvex } from 'mdsvex';
 import mdsvexConfig from './mdsvex.config.js';
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-netlify';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -10,10 +10,7 @@ const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
 	preprocess: [vitePreprocess(), mdsvex(mdsvexConfig)],
-
 	kit: {
-		adapter: adapter({ strict: false })
+		adapter: adapter()
 	}
 };
-
-export default config;
