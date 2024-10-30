c57d78c1e9fa46e7e1bf645c3f3ec9e4d27fd010
DESKTOP-7TJI3IV\DELL <makingideashappenreal@gmail.com>
2024-10-30 20:19:33 +0100

    no message

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,14 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import adapter from '@sveltejs/adapter-netlify';
 
-/** @type {import('vite').UserConfig} */
 const config = {
 	plugins: [sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	},
-	adapter: adapter()
+	kit: {
+		adapter: adapter()
+	}
 };
 
 export default config;
