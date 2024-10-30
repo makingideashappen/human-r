8bc9456063487ec29b3bb70c043439b001eb01f8
DESKTOP-7TJI3IV\DELL <makingideashappenreal@gmail.com>
2024-10-30 18:53:49 +0100

    no message

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,17 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import preprocess from 'svelte-preprocess';
 
 /** @type {import('vite').UserConfig} */
 const config = {
 	plugins: [sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
-	}
+	},
+	preprocess: preprocess({
+		scss: {
+		  prependData: `@import 'src/styles/variables.scss';` // optional, if you use global variables
+		}
+	  }),
 };
 
 export default config;
